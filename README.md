1dc9442c6b714a518e18c8869ba8ba12402fc0db
Mathieu Pithon <craybloge@gmail.com>
2023-06-07 10:06:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CA_Flutter_Project
+ A one week Flutter project in duo for Campus Academy. We are making a travel journal.
---
+++ android/app/src/main/kotlin/com/example/carnet_voyage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carnet_voyage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
