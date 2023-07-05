ddca6c4a7955b572f273aafc370d4e65cdb97789
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2023-07-05 11:21:22 +0200

    création de la navigation

--- lib/main.dart
+++ lib/main.dart
@@ -34,3 +34,66 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  
+  final List<Widget> _children = [
+    Screen1(),
+    Screen2(),
+    Screen3(),
+    Screen4(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mon Application'),
+      ),
+      body: _children[_currentIndex],  // affiche le screen actuel
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            activeIcon: Icon(Icons.home_filled),
+            label: 'home_page',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            activeIcon: Icon(Icons.business_filled),
+            label: 'places_list_page',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            activeIcon: Icon(Icons.school_filled),
+            label: 'planning',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            activeIcon: Icon(Icons.settings_filled),
+            label: 'date_list',
+          ),
+        ],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class Screen1 extends StatefulWidget { HomePage }
+class Screen2 extends StatefulWidget { /* implémentez vos écrans ici */ }
+class Screen3 extends StatefulWidget { /* implémentez vos écrans ici */ }
+class Screen4 extends StatelessWidget { /* implémentez vos écrans ici */ }
