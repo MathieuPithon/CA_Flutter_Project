829ad9f8ada51fd10901b24a04508883e4011a1e
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2023-07-06 14:25:06 +0200

    images!

--- lib/widgets/event_tile.dart
+++ lib/widgets/event_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:carnet_voyage/blocs/places_cubit.dart';
 import 'package:carnet_voyage/model/place.dart';
 import 'package:flutter/material.dart';
@@ -8,35 +10,36 @@ import '../screens/home_page.dart';
 
 class EventTile extends StatelessWidget {
   final Place event;
-  const EventTile(this.event, {Key? key}): super(key: key);
+  const EventTile(this.event, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return  ListTile(
-                          title: Text(event.title),
-                          subtitle: Text(event.address),
-                          trailing: RatingBarIndicator(
-                            rating: event.rating.toDouble(),
-                            itemBuilder: (context, index) =>
-                                const Icon(Icons.star, color: Colors.amber),
-                            itemCount: 5,
-                            itemSize: 20.0,
-                            direction: Axis.horizontal,
-                          ),
-                          onTap: () async {
-                            final result = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HomePage(
-                                  place: event,
-                                ),
-                              ),
-                            );
-                            if (result == true) {
-                              context.read<PlacesCubit>().loadPlaces();
-                            }
-                          },
-                        );
-      
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(event.title),
+      subtitle: event.photoPath.isNotEmpty
+          ? Image.file(File(event.photoPath[0]))
+          : null,
+      trailing: RatingBarIndicator(
+        rating: event.rating.toDouble(),
+        itemBuilder: (context, index) =>
+            const Icon(Icons.star, color: Colors.amber),
+        itemCount: 5,
+        itemSize: 20.0,
+        direction: Axis.horizontal,
+      ),
+      onTap: () async {
+        final result = await Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(
+              place: event,
+            ),
+          ),
+        );
+        if (result == true) {
+          context.read<PlacesCubit>().loadPlaces();
+        }
+      },
+    );
   }
 }
